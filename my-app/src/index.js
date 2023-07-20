c33affb120fb95d263136443ffaf0d528631a7c9
David Rodriguez <david_rodriguez@epam.com>
2023-07-20 13:40:47 -0500

    title and list features added

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const title = React.createElement("h1",
+{ style: { color: '#999', fontSize: '19px' } },
+"Solar system planets");
+
+const planets = (
+  <ul className="planets-list">
+    <li>Mercury</li>
+    <li>Venus</li>
+    <li>Earth</li>
+    <li>Mars</li>
+    <li>Jupiter</li>
+    <li>Saturn</li>
+    <li>Uranus</li>
+    <li>Neptune</li>
+  </ul>
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // To avoid unnecessary HTML nodes and achieve faster rendering (Fragment Component) /David
+  <>
+  {title}
+  {planets}
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
